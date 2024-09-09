475d7a87068f2cc121f030f4c648125b0d7ac144
KORATcs <cgsimon@udc.edu.ar>
2024-09-09 20:02:30 -0300

    instalacion de la aplicacion, templates y desarrollo del modelo mensajes

--- TableroMensajes/settings.py
+++ TableroMensajes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mensajes' #agregamos la aplicacion mensajes en settings.py de TableroMensajes
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'TableroMensajes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], #agrego la carpeta templates 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mensajes/models.py
+++ mensajes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Mensaje (models.Model):
+    texto = models.TextField()
+    remitente = models.CharField(max_length=1000)
+    destinatario = models.CharField(max_length=1000)
+    fecha_hora_envio = models.DateTimeField(default=timezone.now)
