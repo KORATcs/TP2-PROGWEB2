577c8da448a057e584e0650180f47af371d79a75
KORATcs <cgsimon@udc.edu.ar>
2024-09-09 23:42:50 -0300

    modificaciones

--- mensajes/models.py
+++ mensajes/models.py
@@ -8,3 +8,6 @@ class Mensaje (models.Model):
     remitente = models.CharField(max_length=1000)
     destinatario = models.CharField(max_length=1000)
     fecha_hora_envio = models.DateTimeField(default=timezone.now)
+
+def __str__(self):
+    return f"{self.remitente} a {self.destinatario}: {self.texto}"
